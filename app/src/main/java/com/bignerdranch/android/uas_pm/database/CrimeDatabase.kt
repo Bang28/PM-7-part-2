47c34f7afa0236dbaeef7274125547395f518ed6
Bang28 <mhdmasruri9@gmail.com>
2024-01-29 00:45:12 +0700

    database: Enabling TypeConverters

--- app/src/main/java/com/bignerdranch/android/uas_pm/database/CrimeDatabase.kt
+++ app/src/main/java/com/bignerdranch/android/uas_pm/database/CrimeDatabase.kt
@@ -3,11 +3,13 @@ package com.bignerdranch.android.uas_pm.database
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverter
+import androidx.room.TypeConverters
 import com.bignerdranch.android.uas_pm.Crime
 import java.util.*
 
 
 @Database(entities = [ Crime::class ], version=1)
+@TypeConverters(CrimeTypeConverters::class)
 abstract class CrimeDatabase : RoomDatabase() {
 }
 
