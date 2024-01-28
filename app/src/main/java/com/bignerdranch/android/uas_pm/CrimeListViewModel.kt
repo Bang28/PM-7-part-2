ca2100b19e1bf595dd7c3c062e6cd76bea403c69
Bang28 <mhdmasruri9@gmail.com>
2024-01-29 00:20:58 +0700

    CrimelistViewModel: Launching your first coroutine

--- app/src/main/java/com/bignerdranch/android/uas_pm/CrimeListViewModel.kt
+++ app/src/main/java/com/bignerdranch/android/uas_pm/CrimeListViewModel.kt
@@ -1,19 +1,23 @@
 package com.bignerdranch.android.uas_pm
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
 import java.util.*
 
 class CrimeListViewModel : ViewModel() {
     val crimes = mutableListOf<Crime>()
     init {
-        for (i in 0 until 100) {
-            val crime = Crime(
-                id = UUID.randomUUID(),
-                title ="Crime #$i",
-                date = Date(),
-                isSolved = i % 2 == 0
-            )
-            crimes += crime
+        viewModelScope.launch {
+            for (i in 0 until 100) {
+                val crime = Crime(
+                    id = UUID.randomUUID(),
+                    title = "Crime #$i",
+                    date = Date(),
+                    isSolved = i % 2 == 0
+                )
+                crimes += crime
+            }
         }
     }
 }
