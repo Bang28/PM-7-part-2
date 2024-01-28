3825a4b35b122bfbc04c4ebb1d4a720cb79ad51a
Bang28 <mhdmasruri9@gmail.com>
2024-01-28 22:16:48 +0700

    CrimeDetailFragment: Overriding Fragment.onCreate(Bundle?)

--- app/src/main/java/com/bignerdranch/android/uas_pm/CrimeDetailFragment.kt
+++ app/src/main/java/com/bignerdranch/android/uas_pm/CrimeDetailFragment.kt
@@ -1,6 +1,19 @@
 package com.bignerdranch.android.uas_pm
 
+import android.os.Bundle
 import androidx.fragment.app.Fragment
+import java.util.*
 
 class CrimeDetailFragment : Fragment() {
+
+    private lateinit var crime: Crime
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        crime = Crime(
+            id = UUID.randomUUID(),
+            title = "",
+            date = Date(),
+            isSolved = false
+        )
+    }
 }
