9d31431f99debcd503e345c58015ca68d0c4f2e3
Bang28 <mhdmasruri9@gmail.com>
2024-01-29 00:47:53 +0700

    database: Registering the DAO in the database

--- app/src/main/java/com/bignerdranch/android/uas_pm/database/CrimeDatabase.kt
+++ app/src/main/java/com/bignerdranch/android/uas_pm/database/CrimeDatabase.kt
@@ -11,6 +11,7 @@ import java.util.*
 @Database(entities = [ Crime::class ], version=1)
 @TypeConverters(CrimeTypeConverters::class)
 abstract class CrimeDatabase : RoomDatabase() {
+    abstract fun crimeDao(): CrimeDao
 }
 
 class CrimeTypeConverters {
