559d040bde259aa803332e997413da8bb083017d
Bang28 <mhdmasruri9@gmail.com>
2024-01-28 22:19:55 +0700

    CrimeDetailFragment: Listening for CheckBox changes

--- app/src/main/java/com/bignerdranch/android/uas_pm/CrimeDetailFragment.kt
+++ app/src/main/java/com/bignerdranch/android/uas_pm/CrimeDetailFragment.kt
@@ -41,9 +41,14 @@ class CrimeDetailFragment : Fragment() {
             crimeTitle.doOnTextChanged { text, _, _, _ ->
                 crime = crime.copy(title = text.toString())
             }
+
             crimeDate.apply {
                 text = crime.date.toString()
                 isEnabled = false
+            }
+
+            crimeSolved.setOnCheckedChangeListener { _, isChecked ->
+                crime = crime.copy(isSolved = isChecked)
             }
         }
     }
