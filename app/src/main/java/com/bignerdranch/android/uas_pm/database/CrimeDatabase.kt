7f822902ce1f8ee5efdb4b6b236be1b3d548a503
Bang28 <mhdmasruri9@gmail.com>
2024-01-29 00:44:38 +0700

    database: Adding TypeConverter functions

--- app/src/main/java/com/bignerdranch/android/uas_pm/database/CrimeDatabase.kt
+++ app/src/main/java/com/bignerdranch/android/uas_pm/database/CrimeDatabase.kt
@@ -2,8 +2,22 @@ package com.bignerdranch.android.uas_pm.database
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverter
 import com.bignerdranch.android.uas_pm.Crime
+import java.util.*
+
 
 @Database(entities = [ Crime::class ], version=1)
 abstract class CrimeDatabase : RoomDatabase() {
+}
+
+class CrimeTypeConverters {
+    @TypeConverter
+    fun fromDate(date: Date): Long {
+        return date.time
+    }
+    @TypeConverter
+    fun toDate(millisSinceEpoch: Long): Date {
+        return Date(millisSinceEpoch)
+    }
 }
