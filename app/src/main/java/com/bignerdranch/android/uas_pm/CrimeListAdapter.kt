f0d2dae2d08fc4f6b28dbe417873c3f8a4982763
Bang28 <mhdmasruri9@gmail.com>
2024-01-29 00:19:51 +0700

    gradle: Adding coroutines to your project’s build

--- app/src/main/java/com/bignerdranch/android/uas_pm/CrimeListAdapter.kt
+++ app/src/main/java/com/bignerdranch/android/uas_pm/CrimeListAdapter.kt
@@ -1,6 +1,7 @@
 package com.bignerdranch.android.uas_pm
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
