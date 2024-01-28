2212cd8c2f50dde9d31b7cf28ec5b59b8b7a065f
Bang28 <mhdmasruri9@gmail.com>
2024-01-28 22:06:31 +0700

    Crime: Adding the Crime data class

--- app/src/main/java/com/bignerdranch/android/uas_pm/Crime.kt
+++ app/src/main/java/com/bignerdranch/android/uas_pm/Crime.kt
@@ -1,4 +1,10 @@
 package com.bignerdranch.android.uas_pm
 
-class Crime {
-}
+import java.util.*
+
+data class Crime(
+    val id: UUID,
+    val title: String,
+    val date: Date,
+    val isSolved: Boolean
+)
