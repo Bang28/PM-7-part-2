a6beb4ab947411fb0066b2908fdd5fed13033fe1
Bang28 <mhdmasruri9@gmail.com>
2024-01-28 22:18:05 +0700

    CrimeDetailFragment: Overriding onCreateView(…)

--- app/src/main/java/com/bignerdranch/android/uas_pm/CrimeDetailFragment.kt
+++ app/src/main/java/com/bignerdranch/android/uas_pm/CrimeDetailFragment.kt
@@ -1,10 +1,16 @@
 package com.bignerdranch.android.uas_pm
 
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.bignerdranch.android.uas_pm.databinding.FragmentCrimeDetailBinding
 import java.util.*
 
 class CrimeDetailFragment : Fragment() {
+
+    private lateinit var binding: FragmentCrimeDetailBinding
 
     private lateinit var crime: Crime
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,5 +21,16 @@ class CrimeDetailFragment : Fragment() {
             date = Date(),
             isSolved = false
         )
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding =
+            FragmentCrimeDetailBinding.inflate(layoutInflater, container,
+                false)
+        return binding.root
     }
 }
