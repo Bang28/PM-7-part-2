b5a4eb42c5b2285756b57f4612dcee68cc2b1aad
Bang28 <mhdmasruri9@gmail.com>
2024-01-29 00:47:02 +0700

    database: Adding database query functions

--- app/src/main/java/com/bignerdranch/android/uas_pm/database/CrimeDao.kt
+++ app/src/main/java/com/bignerdranch/android/uas_pm/database/CrimeDao.kt
@@ -1,7 +1,14 @@
 package com.bignerdranch.android.uas_pm.database
 
 import androidx.room.Dao
+import androidx.room.Query
+import com.bignerdranch.android.uas_pm.Crime
+import java.util.*
 
 @Dao
 interface CrimeDao {
+    @Query("SELECT * FROM crime")
+    suspend fun getCrimes(): List<Crime>
+    @Query("SELECT * FROM crime WHERE id=(:id)")
+    suspend fun getCrime(id: UUID): Crime
 }
