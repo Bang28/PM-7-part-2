b641362fab32d3d411a1bba035dc9fa84c692ee5
Bang28 <mhdmasruri9@gmail.com>
2024-01-29 22:01:15 +0700

    CrimeDao: Creating a Flow from your database

--- app/src/main/java/com/bignerdranch/android/uas_pm/database/CrimeDao.kt
+++ app/src/main/java/com/bignerdranch/android/uas_pm/database/CrimeDao.kt
@@ -3,13 +3,14 @@ package com.bignerdranch.android.uas_pm.database
 import androidx.room.Dao
 import androidx.room.Query
 import com.bignerdranch.android.uas_pm.Crime
-import java.util.*
 import kotlinx.coroutines.flow.Flow
+import java.util.*
+
 
 @Dao
 interface CrimeDao {
     @Query("SELECT * FROM crime")
-    suspend fun getCrimes(): List<Crime>
+    suspend fun getCrimes(): Flow<List<Crime>>
 
     @Query("SELECT * FROM crime WHERE id=(:id)")
     suspend fun getCrime(id: UUID): Crime
